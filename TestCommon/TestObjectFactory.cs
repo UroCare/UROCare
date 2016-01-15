95a088701ec510cc9e7e71b38df3d1649e22710a
vivekdixit <vivekdixit05@gmail.com>
2016-01-15 23:05:25 +0530

    Add missing file

---
+++ TestCommon/TestObjectFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FakeItEasy;
+using System.Collections.Generic;
+
+using SHC.UROCare.UROCareDataModel;
+using System.Threading;
+
+namespace SHC.UROCare.URCareBusinessObjectsTest
+{
+    public class TestObjectFactory
+    {
+        public static IUROCareEntities GetMockedDataContext()
+        {
+            
+            IUROCareEntities uroCareEntity = A.Fake<IUROCareEntities>();
+            A.CallTo(() => uroCareEntity.User_Master).Returns(new MockObjectSet<User_Master>());
+            
+            A.CallTo(() => uroCareEntity.Patient_Info).Returns(new MockObjectSet<Patient_Info>());
+            return uroCareEntity;
+        }
+
+        public static void SetFakedDataContext()
+        {
+            Thread.SetData(Thread.GetNamedDataSlot("dataContext"),GetMockedDataContext());
+        }
+    }
+}
